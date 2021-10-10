71b40260cfca01ad86cb8a1c74afe93870515f98
Hammad Shahid <71876842+dr-wajahat@users.noreply.github.com>
2021-10-10 18:41:57 +0500

    Add files via upload

---
+++ diamond.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	int rows;
+  
+	int temp;
+  
+	cout << "Enter the number of rows: ";
+  
+ 
+	cin >> rows;
+  
+	int row = 1, i, j, k;
+  
+	temp = 1;  
+ 
+	for (i = rows / 2; i > 0; i--){				
+		for (j = 1; j <= i; j++){
+			cout << " ";
+		}
+       
+		for (j = 1; j <= row; j++){
+			cout << temp;
+			temp = temp + 1;
+			cout << " ";
+		}
+      
+	cout << "\n";	
+	row++;
+	}
+  
+	for (i = 0; i <= rows / 2; i++){
+		for (j = 1; j <= i; j++){
+			cout << " ";
+
+		}
+		for (j = row; j >= 1; j--){		
+			cout << temp;  
+			temp = temp + 1;
+			cout << " ";
+		}
+      
+	cout << "\n";
+	row--;
+	}
+  
+	return 0; 
+}
+
+
